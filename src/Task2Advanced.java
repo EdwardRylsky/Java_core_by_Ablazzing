55b6d777274cb772cd4e58904ca71683ccaddd41
Edward <edwardrylsky@gmail.com>
2022-06-08 23:10:30 +0500

    Task2Advanced

---
+++ src/Task2Advanced.java
@@ -0,0 +1,8 @@
+/*
+    Посчитать (a+b)^2 = ?, при a=3, b=5
+ */
+public class Task2Advanced {
+    public static void main(String[] args) {
+        System.out.println("При a=3, b=5 уравнение (a+b)^2 = " +(int) Math.pow((3+5),2));
+    }
+}
