ab9baa91ca8fc758fba81e9bf99749fcb313aac3
Edward <edwardrylsky@gmail.com>
2022-06-08 23:02:10 +0500

    Task2

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,16 @@
+/*
+    Создать переменные с ростом, весом.
+    Произвести расчет индекса массы тела (вес) / (рост * рост) используя переменные, вывести на экран
+    Пример результата вывода на экран:
+    21.0
+ */
+
 public class Task2 {
+    public static void main(String[] args) {
+        double weight = 72;
+        double height = 1.73;
+        double BMI = weight/(height * height);
+
+        System.out.printf("Индекс массы тела равен: %.2f", BMI);
+    }
 }
