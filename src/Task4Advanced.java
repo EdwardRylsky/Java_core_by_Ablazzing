846362cbfe23b0f061c1c58e7c86ac393a4fe733
Edward <edwardrylsky@gmail.com>
2022-06-08 23:25:26 +0500

    Task4Advanced

---
+++ src/Task4Advanced.java
@@ -0,0 +1,10 @@
+/*
+    В слове "Hello world!" заменить все l на r, сделать все буквы заглавными, выбрать первые 8 символов
+ */
+public class Task4Advanced {
+    public static void main(String[] args) {
+        String str = "Hello world!";
+
+        System.out.println(str.replace('l', 'r').toUpperCase().substring(0,7));
+    }
+}
