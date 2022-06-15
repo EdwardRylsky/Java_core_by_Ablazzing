e005099aa7c62a9a94b925db4afb09d3958b42bc
Edward <edwardrylsky@gmail.com>
2022-06-15 23:53:31 +0500

    Homework2 Task1

---
+++ src/Homework2/Task1.java
@@ -0,0 +1,10 @@
+package Homework2;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + "a ");
+        }
+    }
+}
