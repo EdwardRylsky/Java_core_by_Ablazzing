4fe557cdcbd5b4053cd3641b1324e410f45ef579
Edward <edwardrylsky@gmail.com>
2022-06-21 01:41:42 +0500

    Homework3 Task1Expert

---
+++ src/Homework3/TaskExpert/TasksExpert.java
@@ -0,0 +1,82 @@
+package Homework3.TaskExpert;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TasksExpert {
+    public static void main(String[] args) throws Exception{
+        final String DIRECTORY_PATH = "C:\\Users\\Edward\\IdeaProjects\\Java_core_by_Ablazzing\\resource\\";
+        final String FILE_MASK = "report_\\d{2}_\\d{4}.txt";
+        final String STORE_NAME = "pyterochka";
+
+        //получаем список подходящих файлов
+        List<File> needFileNamesList = readFilesFromDirByMask(DIRECTORY_PATH, FILE_MASK);
+
+        //рассчитываем прибыль по месяцам по магазину pyterochka
+        calculateStoreProfitByAllReports(needFileNamesList, STORE_NAME);
+
+    }
+
+    private static void calculateStoreProfitByAllReports(List<File> files, String store_name) throws Exception{
+        System.out.printf("Прибыль по магазину %s по месяцам", store_name);
+        System.out.println();
+        for (File file : files) {
+            calculateStoreProfitByReport(file, store_name);
+        }
+    }
+
+    //pyterochka;299922.00;323333.02;21/01/2012
+    private static void calculateStoreProfitByReport(File file, String store_name) throws Exception {
+        double totalIncomes = 0;
+        double totalOutcomes = 0;
+        String currentMonth =  file.getName().substring(7, 14).replace('_', '.');
+
+        List<String> storeOperationsList = readStoreOperations(file, store_name);
+
+        for (String operation : storeOperationsList) {
+                String[] resultsSplit = operation.split(";");
+
+                totalIncomes += Double.parseDouble(resultsSplit[1]);
+                totalOutcomes += Double.parseDouble(resultsSplit[2]);
+        }
+
+        System.out.printf("%s : %.2f", currentMonth, (totalIncomes-totalOutcomes));
+        System.out.println();
+    }
+
+
+
+    private static List<String> readStoreOperations(File file, String store_name) throws Exception {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            while (reader.ready()) {
+                String newLine = reader.readLine();
+
+                if (newLine.startsWith(store_name)) {
+                    result.add(newLine);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static List<File> readFilesFromDirByMask(String directoryPath, String regex) {
+        List<File> result = new ArrayList<>();
+
+        File directory = Paths.get(directoryPath).toFile();
+
+        for (final File fileEntry : directory.listFiles()) {
+            if (!fileEntry.isDirectory()) {
+                if (fileEntry.getName().matches(regex)) {
+                    result.add(fileEntry);
+                }
+            }
+        }
+        return result;
+    }
+}
